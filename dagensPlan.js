1e2eb5aa19862980e3fee1c5c2c4a873af076330
Rebecka Kinn <rebeckaki@getacademy.no>
2024-09-27 09:24:33 +0200

    Update dagensPlan.js

--- dagensPlan.js
+++ dagensPlan.js
@@ -1,14 +1,22 @@
 /*
 
+TIL INFO: 
+Dette blir IKKE tatt opp, MEN det ligger en video på 
+Moodle Emne 2 hvor det er enda et eksempel på å lage 
+modell utifra skjermbilder. 
 
 
 
 DAGENS PLAN:
-- Lage modell på Emne 2 - måten
+- Lage modell på Emne 2 - måten 
+    (Hva, Hvordan, Hvorfor)
 
-- Hvordan lage en modell utifra skjermbilder
+- Hvordan lage en modell utifra skjermbilder 
+    (Kom med forslag underveis!)
 
 - Gå i team og lag en modell utifra skjermbildene dere lagde i går
+    (Be kodelærer komme inn eller 
+    send den når dere er feridg for tilbakemelding)
 
 
 
@@ -22,6 +30,9 @@ DAGENS PLAN:
 
 
 MODELL EMNE 2: 
+
+Vi deler opp modellen vår i 3 deler:
+
 - APP
     Det som håndterer siden. Eks: 
     - språk
