59458d44ef07ba82deee585a9808448d45ff8285
Rebecka Kinn <rebeckaki@getacademy.no>
2024-09-27 08:55:13 +0200

    added plan for the day

---
+++ dagensPlan.js
@@ -0,0 +1,48 @@
+/*
+
+
+
+
+DAGENS PLAN:
+- Lage modell på Emne 2 - måten
+
+- Hvordan lage en modell utifra skjermbilder
+
+- Gå i team og lag en modell utifra skjermbildene dere lagde i går
+
+
+
+
+
+
+
+
+
+
+
+
+MODELL EMNE 2: 
+- APP
+    Det som håndterer siden. Eks: 
+    - språk
+    - darkmode
+    - de forskjellige viewsene (pages)
+    - document.getelementbyid til hoved-div (div id app)
+
+- INPUT
+    Det brukeren kan putte inn av informasjon (data). 
+    Det skal "mellomlagre" verdiene som skal senere bli lagt inn 
+    eller endre på verdiene i data. Eks:
+    - Input-felt
+    - Handlekurv
+
+- DATA
+    "Databasen" du skal legge det som vises på siden som kan variere. 
+    NB: Siden dette er en "front-end", så bruker vi en ferdig laget
+    "dummy-database" for å vise hvordan det ville sett ut med en 
+    database i senere tid (ettersom det er en prototype). Eks: 
+    - Gjøremål
+    - Varer i butikk
+    - Brukere / Profiler
+
+*/
--- model2.js
+++
@@ -1,7 +0,0 @@
-/*
-DAGENS PLAN:
-- Lage modell på Emne 2 - måten
-- Hvordan lage en modell utifra skjermbilder
-- Gå i team og lag en modell utifra skjermbildene dere lagde i går
-
-*/
